54856a12fc67f6b3523283c9d7b824238a12ce0c
Edmilson <scovinowork@gmail.com>
2021-10-03 16:17:49 -0300

    primeiro test

---
+++ src/test/java/PreAcessoSalaTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PreAcessoSalaTest {
+
+    @Test
+    void deveRetornarQuantidadeJogadoresNaSala(){
+        SalaDeJogo sala = new SalaDeJogo(
+                new Jogador("XeronildoXD2014", true),
+                new Jogador("xXBalaXx", false),
+                new Jogador("JoaninhaS2", false),
+                new Jogador("PedroS10", true),
+                new Jogador("XXRobsonYY", true),
+                new Jogador("KL78221", true)
+        );
+        assertEquals(4, PreAcessoSala.mostrarQuantidadeDeJogadores(sala));
+    }
+}
